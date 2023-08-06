bc776a67000679631cd981dc343a091314076b3e
kibreab <kibreabteshome1993@gmail.com>
2023-08-07 01:02:05 +0300

    some update

--- src/Home.js
+++ src/Home.js
@@ -2,41 +2,45 @@ import { useEffect, useState } from "react"
 import BlogList from "./BlogList";
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState(null)
     const [isLoading,setIsLoading]=useState(true)
+    const [error,setError]=useState()
+    
+    const apiUrl='  http://localhost:8000/blogs1'
 
-    const apiUrl='  http://localhost:8000/blogs'
+
+    useEffect(()=>{
+      setTimeout(()=>{
+        fetch(apiUrl)
+        .then(res=>{
+          if(!res.ok){
+            throw Error("couldn't fetch data from that resource")
+          }
+  
+          return res.json()
+        })
+        .then(data=>{
+          setBlogs(data)
+          setIsLoading(false)
+        })
+        .catch(error=>{
+          setError(error.message)
+          setIsLoading(false)
+        })
+      },1000)
+     
+    })
+
  
-    
-  useEffect(()=>{
-    setTimeout(()=>{
-      fetch(apiUrl)
-    .then(res=>{
-      console.log(res)
-      return res.json()
-
-    })
-    .then(data=>{
-      setBlogs(data)
-      setIsLoading(false)
-    })
-    .catch(error=>{
-      console.log("error fetching data",error)
-      setIsLoading(false)
-    })
-    },1000)
-    
-  }
-  )
+  
    
      
-      
-
-
+    
     return ( 
         <div className="home">
-          {isLoading?<p>Loading...</p>:
-           blogs && <BlogList blogs={blogs}  title="All blogs!"/>}
+          {isLoading&&<div>Loading...</div>}
+          <p>{error}</p>
+          {blogs&&<BlogList blogs={blogs}/>}
         </div>
      );
 }
