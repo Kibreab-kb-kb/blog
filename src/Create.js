5ba91d97c4d72f7a97336aac799d788a0b30d2b7
kibreab <kibreabteshome1993@gmail.com>
2023-08-08 23:42:33 +0300

    adding router

---
+++ src/Create.js
@@ -0,0 +1,8 @@
+const Create = () => {
+    return (  
+
+        <h2>Add a New Blog</h2>
+    );
+}
+ 
+export default Create;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return ( 
         <div className="navbar flex border w-96 ">
             <h1>The Kb blog</h1>
+
             <div className="links">
-                <a href='/'>Home</a>
-                <a href='/' style={{
-                    color:"white",
-                    backgroundColor:'#f1356d',
-                    borderRadius:'8px'
-                }}>NewBlog</a>
+                <Link to ='/'>Home</Link>
+                <Link to ='/create'>NewBlog</Link>
             </div>
         </div>
      );
